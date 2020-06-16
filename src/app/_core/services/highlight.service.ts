98dbe0349367962d6f6902904edfe7d76485c031
MacGregor Thompson <mhthompson86@gmail.com>
2020-06-16 11:16:44 -0400

    another example

--- src/app/_core/services/highlight.service.ts
+++ src/app/_core/services/highlight.service.ts
@@ -14,6 +14,7 @@ import 'prismjs/components/prism-css';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-typescript';
 import 'prismjs/components/prism-scss';
+import 'prismjs/components/prism-markdown';
 
 declare var Prism: any;
 
--- src/app/articles/articles.module.ts
+++ src/app/articles/articles.module.ts
@@ -7,12 +7,20 @@ import { SharedModule } from '../_shared/shared.module';
 import { ExampleOneComponent } from './async-javascript/example-one/example-one.component';
 import { ExampleTwoComponent } from './async-javascript/example-two/example-two.component';
 import { RouterModule } from '@angular/router';
+import { ExampleThreeComponent } from './async-javascript/example-three/example-three.component';
+import { ExampleFourComponent } from './async-javascript/example-four/example-four.component';
+import { ExampleFiveComponent } from './async-javascript/example-five/example-five.component';
+import { ExampleSixComponent } from './async-javascript/example-six/example-six.component';
 
 @NgModule({
   declarations: [
     AsyncJavascriptComponent,
     ExampleOneComponent,
-    ExampleTwoComponent
+    ExampleTwoComponent,
+    ExampleThreeComponent,
+    ExampleFourComponent,
+    ExampleFiveComponent,
+    ExampleSixComponent
   ],
   imports: [
     CommonModule,
